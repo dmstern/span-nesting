95515cc0ea30018d674992bc5a78930b0808b092
MorRue <m.rueber@web.de>
2025-01-23 10:30:17 +0100

    Refactor nest function

--- nest-elements.ts
+++ nest-elements.ts
@@ -4,211 +4,125 @@
  *
  * */
 
-interface ClassGroupMapping {
-    classKey: string;
-    spans: Element[];
-}
-
-function sortSpansBySize(a: ClassGroupMapping, b: ClassGroupMapping) {
-    const firstSize = a.spans.length;
-    const secondSize = b.spans.length;
-
-    if (firstSize < secondSize) {
-        return -1;
-    }
-
-    if (firstSize > secondSize) {
-        return 1;
-    }
-
-    return 0;
-}
-
-function groupByClass(spans: Element[]): ClassGroupMapping[] {
-    const allClasses = [...spans].map((span) => [...span.classList]).flat();
-    const uniqueClasses = new Set(allClasses);
-    const groupedLogicSpans = [...uniqueClasses].map((cssClass) => {
-        return {
-            classKey: cssClass,
-            spans: [...spans].filter((span) => span.classList.contains(cssClass)),
-        };
-    });
-
-    return cleanupGroupings(groupedLogicSpans);
-}
-
-function containSpansWithClass(classGroupMapping: ClassGroupMapping, classKey: string) {
-    return classGroupMapping.spans.some((span) => span.classList.contains(classKey));
-}
-
-function cleanupGroupings(groupedLogicSpans: ClassGroupMapping[]): ClassGroupMapping[] {
-    const result: ClassGroupMapping[] = [];
-
-    groupedLogicSpans.forEach((groupedLogicSpan) => {
-        const allMatchingGroups = groupedLogicSpans.filter((group) =>
-            containSpansWithClass(group, groupedLogicSpan.classKey),
-        );
-        const sorted = allMatchingGroups.sort(sortSpansBySize);
-        const biggest = sorted.at(-1);
-        const resultContainsSpan = result.some((group) => containSpansWithClass(group, groupedLogicSpan.classKey));
-
-        if (!resultContainsSpan && biggest) {
-            result.push(biggest);
-        }
-    });
-
-    return result;
-}
 
 function haveOverlappingClass(firstElement: Element, secondElement: Element): boolean {
     return [...firstElement.classList].some((clazz) => secondElement.classList.contains(clazz));
 }
 
-function getNextElementsTillOnlySingleClass(elements: Element[], classKey: string): Element[] {
-    const nextElements: Element[] = [elements[0]];
-    for (let i = 1; i < elements.length; i++) {
-        if (elements[i].classList.length > 1) {
-            const elementWithoutClass = elements[i].cloneNode(true) as Element;
-            elementWithoutClass.classList.remove(classKey);
-            nextElements.push(elementWithoutClass);
-        } else {
-            return nextElements;
-        }
-    }
-    return nextElements;
-}
-
-function nestGroupedSpans(classKey: string, flatSpans: Element[]) {
-    const spanWrapper = document.createElement("span");
-    spanWrapper.classList.add(classKey);
-
-    flatSpans.forEach((flatSpan) => {
-        const hasOnlyClassKeyClass = flatSpan.classList.length === 1 && flatSpan.classList.contains(classKey);
-
-        if (hasOnlyClassKeyClass) {
-            const textNode = document.createTextNode(flatSpan.innerHTML);
-            spanWrapper.append(textNode);
-        } else {
-            flatSpan.classList.remove(classKey);
-            const spanWrapperContainsFlatSpan = haveOverlappingClass(spanWrapper, flatSpan);
-            const spanWrapperChildContainsFlatSpan = [...spanWrapper.children].some((child) =>
-                haveOverlappingClass(child, flatSpan),
-            );
-
-            if (!spanWrapperContainsFlatSpan && !spanWrapperChildContainsFlatSpan) {
-                // create new wrapper, which has the following conditions:
-                // - contains everything, which comes after flatspan till there is a span with only class = "classkey"
-                // - classKey gets removed as class from all other spans
-                const index = flatSpans.indexOf(flatSpan);
-                const nextSiblings = getNextElementsTillOnlySingleClass(flatSpans.slice(index), classKey);
-                // we have to nest even deeper
-                const nestingIsFinished = nextSiblings.length === 1 && nextSiblings[0].classList.length === 1;
-                if (nestingIsFinished) {
-                    spanWrapper.append(flatSpan.cloneNode(true));
-                } else {
-                    const newNode = document.createElement("p");
-                    newNode.append(...nextSiblings);
-                    const nested = nestElements(newNode); //todo check for recursion deepness?
-                    spanWrapper.append(...nested.childNodes);
-                }
-            }
-        }
-    });
-
-    return spanWrapper;
-}
-
-function assembleNewParagraph(node: Element, nestedSpans: HTMLSpanElement[]): Element {
-    const newNode = document.createElement("p");
-
-    [...node.childNodes].forEach((child) => {
-        const isTextNode = !(child instanceof Element);
-
-        if (isTextNode) {
-            newNode.append(child.cloneNode());
-        } else {
-            replaceSimilarSpansWithNestedSpan(newNode, child, nestedSpans);
-        }
-    });
-    // If there are no ChildElements (in the deepest or lowest idk?) we still need to add the nestedSpans
-    replaceSimilarSpansWithNestedSpan(newNode, null, nestedSpans);
-
-    return newNode;
-}
-
-function replaceSimilarSpansWithNestedSpan(node: Element, child: Element | null, nestedSpans: HTMLSpanElement[]) {
-    const isSpanAlreadyAdded =
-        node.childNodes.length > 1 &&
-        child !== null &&
-        [...child.classList].every((currentClass) => {
-            return !!node.querySelector(`.${currentClass}`);
-        });
-
-    if (isSpanAlreadyAdded) {
-        return;
-    }
-
-    const nextNestedSpan = nestedSpans.shift();
-
-    if (nextNestedSpan) {
-        node.append(nextNestedSpan.cloneNode(true));
-    }
-}
-
-function mergeSingleElement(span : HTMLSpanElement) : Element {
-    let returnVal = document.createElement("span")
-    let currentSpan = returnVal
-    let nestedSpan :HTMLSpanElement
-    span.classList.forEach((className) => {
-        nestedSpan = document.createElement("span")
-        nestedSpan.classList.add(className)
-        currentSpan.append(nestedSpan);
-        currentSpan = nestedSpan
-    })
-    currentSpan.innerHTML = span.innerHTML
-    return returnVal.firstElementChild;
-
-}
-
-function getOverlappingClass(elements : Element[]) {
+function getOverlappingClass(elements: HTMLSpanElement[]): string | undefined {
     return [...elements[0].classList.values()].find((clazz) => elements.every((el) => el.classList.contains(clazz)));
 }
-function mergeElements(elements:Element[]) {
-    let returnVal : HTMLSpanElement = document.createElement("span");
-    const overLappingClass = getOverlappingClass(elements)
-    returnVal.classList.add(overLappingClass)
-    elements.forEach((el) =>{
-        el.classList.remove(overLappingClass)
-            if(el.classList.length === 0) {
-                returnVal.innerHTML += el.innerHTML
-            } else {
-                returnVal.append(el.cloneNode(true))
-            }
-    })
-    return returnVal;
 
+function getSpansWithOverlappingClasses(element: HTMLSpanElement, elements: NodeListOf<HTMLSpanElement>) {
+    const spansWithSameClasses: HTMLSpanElement[] = [];
+    elements.forEach((span) => {
+        if (haveOverlappingClass(element, span)) {
+            spansWithSameClasses.push(span);
+        }
+    });
+    return spansWithSameClasses;
 }
 
+function appendElement(baseElement: HTMLSpanElement, appendElement: HTMLSpanElement) {
+    if (appendElement.classList.length === 0) {
+        baseElement.innerHTML += appendElement.innerHTML;
+    } else {
+        baseElement.append(appendElement.cloneNode(true));
+    }
+}
+
+function replaceMultipleSpansWithSingleSpan(spans: HTMLSpanElement[], mergedSpan: HTMLSpanElement) {
+    spans[0].replaceWith(mergedSpan);
+    for (let i = 1; i < spans.length; i++) {
+        spans[i].remove();
+    }
+}
+
+function mergeSingleElement(span: HTMLSpanElement): HTMLSpanElement {
+    const returnVal = document.createElement("span") as HTMLSpanElement;
+    let currentSpan: HTMLSpanElement = returnVal;
+    let nestedSpan: HTMLSpanElement;
+    span.classList.forEach((className) => {
+        nestedSpan = document.createElement("span");
+        nestedSpan.classList.add(className);
+        currentSpan.append(nestedSpan);
+        currentSpan = nestedSpan;
+    });
+    currentSpan.innerHTML = span.innerHTML;
+    return returnVal.firstElementChild as HTMLSpanElement;
+}
+
+function mergeElements(elements: HTMLSpanElement[]) {
+    const returnVal: HTMLSpanElement = document.createElement("span");
+    const overLappingClass = getOverlappingClass(elements);
+    if (overLappingClass) {
+        returnVal.classList.add(overLappingClass);
+        elements.forEach((element) => {
+            element.classList.remove(overLappingClass);
+            appendElement(returnVal, element);
+        });
+    } else {
+        console.error("At this there should always be an overlapping class.");
+    }
+
+    return returnVal;
+}
+
+
+/**
+ * This function will merge children-span-elements of the parents if they have an overlapping class.
+ * It transforms html of the form
+ * ```
+ * <p>
+ *   Text
+ *   <span class=a>A</span>
+ *   <span class=a b>AB</span>
+ *   Text
+ *    <span class=c d e>CDE</span>
+ *    Text
+ * </p>
+ * ```
+ * to html of the form
+ * ```
+ * <p>
+ *   Text
+ *    <span class=a>A
+ *      <span class=b>AB</span>
+ *    </span>
+ *   Text
+ *   <span class=c>
+ *    <span class=d>
+ *     <span class=d>CDE</span>
+ *    </span>
+ *   </span>
+ *   Text
+ * </p>
+ *
+ * ```
+ * It follows the following logic:
+ * Grab all spans which there are. Take the first one and do the following:
+ * Collect all the spans which have an overlapping class.We know that the spans with overlapping classes are all
+ * direct siblings of each other and all have a common class.
+ * In case there is no other span which has an overlapping class, we have to merge the currentSpan just in itself.
+ * If there are other spans with an overlapping class, we take all the spans and merge them in one span, which has
+ * the overlapping class as classAttribute.
+ * Now the structure of the parent may have changed, but we know, that the currentSpan is correctly nested, and we can
+ * take the next span and do the above described procedure till we have nested all spans.
+ *
+ * @param parent
+ */
 function nestElements(parent: Element) {
     let spans = parent.querySelectorAll("span");
-    let i = 0
-    while(i<spans.length){
-        const spansWithSameClasses : Element[] = []
+    for (let i = 0; i < spans.length; i++) {
         const currentSpan = spans[i];
-        spans.forEach((span) => {
-            if(haveOverlappingClass(currentSpan, span)) {
-            spansWithSameClasses.push(span)
-            }
-        })
-        if(spansWithSameClasses.length > 1) {
-            currentSpan.replaceWith(mergeElements(spansWithSameClasses))
-            for (let j =1;j<spansWithSameClasses.length;j++) {
-                spans[i+j].remove()
-            }
+        let mergedSpan: HTMLSpanElement;
+        const spansWithSameClasses: HTMLSpanElement[] = getSpansWithOverlappingClasses(currentSpan, spans);
+        if (spansWithSameClasses.length > 1) {
+            mergedSpan = mergeElements(spansWithSameClasses);
         } else {
-            currentSpan.replaceWith(mergeSingleElement(currentSpan))
-            i++
+            mergedSpan = mergeSingleElement(currentSpan);
         }
+        replaceMultipleSpansWithSingleSpan(spansWithSameClasses, mergedSpan);
         spans = parent.querySelectorAll("span");
     }
     return parent;
